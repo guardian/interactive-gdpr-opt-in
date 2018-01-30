a3a5cac4764aaf314930f376db7635db09002f83
Laura <laura.gonzalez@guardian.co.uk>
2018-01-30 14:55:51 +0000

    fixes

--- js/module/faq-footer.js
+++ js/module/faq-footer.js
@@ -1,15 +1,43 @@
-const template =
-`
-	<a class="oi-cta oi-cta--secondary--purple">
-		That was helpful
-	</a>
-	<a class="oi-cta oi-cta--secondary--purple">
-		I hate everything that stands for
-	</a>
-`
+"use strict";
+
+const VOTE_YAY = 'helpful';
+const VOTE_NAY = 'useless';
+
+const TEXT_YAY = 'That was helpful'
+const TEXT_NAY = 'That was useless'
+const TEXT_TKS = 'Thank you for the feedback!'
+
+const makeVoteButton = (type, title) => {
+	const $button = document.createElement('button');
+	$button.className = 'oi-cta oi-cta--secondary--purple';
+	$button.dataset.linkName = `optin : ${type} : ${title}`
+	$button.innerText = type === VOTE_YAY
+		? TEXT_YAY
+		: TEXT_NAY
+
+	return $button;
+}
+
+const $thanks = (()=>{
+	const $button = document.createElement('button');
+	$button.className = 'oi-cta oi-cta--paragraph';
+	$button.innerText = TEXT_TKS
+	return $button;
+})()
 
 const inject = ($element) => {
-	$element.innerHTML = template;
+
+	const $title = $element.closest('.oi-faq').querySelector('.oi-faq__header').innerText;
+	[VOTE_YAY, VOTE_NAY].forEach(vote => {
+		const $button = makeVoteButton(vote, $title);
+		$button.addEventListener('click', () => {
+			$element.innerHTML = ''
+			$element.appendChild($thanks);
+		})
+		$element.appendChild(
+			$button
+		)
+	})
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: path.resolve(path.join(__dirname, 'js', 'main.js')),
-  output: {
-    path: path.resolve(path.join(__dirname, 'public')),
-    filename: 'main.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
-    ]
-  }
+	entry: path.resolve(path.join(__dirname, 'js', 'main.js')),
+	output: {
+		path: path.resolve(path.join(__dirname, 'public')),
+		filename: 'main.js'
+	},
+	devtool: "source-map",
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			minimize: true,
+			sourceMap: true,
+			compress: {
+				drop_debugger: false
+			}
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+				}
+			}
+		]
+	}
 }
