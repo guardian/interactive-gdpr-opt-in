7e716632ac591eb29928c5a6030fe94e2c49297f
Laura <laura.gonzalez@guardian.co.uk>
2018-02-02 17:16:18 +0000

    remove extra js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 import {inject as injectFaq} from './module/faq';
-import {inject as injectFaqFooter} from './module/faq-footer';
+//import {inject as injectFaqFooter} from './module/faq-footer';
 import {inject as injectFaqMore} from './module/faq-more';
-import {inject as injectResend} from './module/resend-widget';
+//import {inject as injectResend} from './module/resend-widget';
 
 const injectables = [
 	['.oi-faq__footer', injectFaqFooter],
-	['.oi-faq-more', injectFaqMore],
 	['.oi-faq', injectFaq],
-	['.oi-resend-widget', injectResend]
 ]
 
 const onReady = () => {
